82e5ae7258b893c04fd64e342b5ce225acb15de4
Leeo <li4hao@yeah.net>
2016-11-04 16:55:09 +0800

    Initial commit

---
+++ src/com/lish/demo/helloworld.java
@@ -0,0 +1,10 @@
+package com.lish.demo;
+
+/**
+ * Created by lisho on 16/11/4.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
